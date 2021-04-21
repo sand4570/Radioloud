14dc81779dde147c0563a1d29757481be7eec839
kami7277@stud.kea.dk <kami7277@stud.kea.dk>
2021-04-21 14:40:42 +0200

    seneste nyt

--- template-parts/front.php
+++ template-parts/front.php
@@ -1,21 +1,25 @@
-<?php
-/**
- * The template for displaying singular post-types: posts, pages and user-defined custom post types.
- *
- * @package HelloElementor
- */
+<html>
+<h2>Seneste nyt</h2>
+<div>
+    <img src="../images/fr%C3%A5s.jpg" alt="Frås-logo">
+    <p>Frås: Episode #120</p>
+</div>
+<div>
+    <img src="../images/ligestilling.jpg" alt="Ligestilling-logo">
+    <p>Langt til ligestilling: Episode #73</p>
+</div>
+<div>
+    <img src="../images/america.jpg" alt="America-logo">
+    <p>Made in America: Episode #54</p>
+</div>
+<div>
+    <img src="../images/george.jpg" alt="GeorgeFloyd-logo">
+    <p>Geroge Floyd: Episode #13</p>
+</div>
+<div>
+    <img src="../images/alverden.jpg" alt="Hvadialverden-logo">
+    <p>Hvad i alverden: Episode #39</p>
+</div>
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
-}
-?>
-<?php
-while ( have_posts() ) : the_post();
-	?>
 
-<main <?php post_class( 'site-main' ); ?> role="main">
-    Front page 
-</main>
-
-	<?php
-endwhile;
+</html>
