b4531f7cf1352c478a75d8c08075b122d0777845
kami7277@stud.kea.dk <kami7277@stud.kea.dk>
2021-04-21 14:56:00 +0200

    forside

--- template-parts/front.php
+++ template-parts/front.php
@@ -1,5 +1,5 @@
 <html>
-<h2>Seneste nyt</h2>
+<h3>Seneste nyt</h3>
 <div>
     <img src="../images/fr%C3%A5s.jpg" alt="Frås-logo">
     <p>Frås: Episode #120</p>
@@ -21,5 +21,22 @@
     <p>Hvad i alverden: Episode #39</p>
 </div>
 
+<div>
+    <img src="../images/deunge.jpg" alt="De-unge-billede">
+    <h2>De unge ved verdens ende</h2>
+    <p>En række stærke og intelligente grønlændere, giver deres besyv på ungdomslivet i Grønland.</p>
+</div>
+<h1>I fokus denne måned...</h1>
+<p>I denne måned sætter Radio Loud fokus på kulturforskelle. I sær ser vi det vigtigt at snakke om hvordan unge trives og begår sig i forskellige kulture og samfund rundt omkring i verden. <br>Kig med herunder, for vores forslag til hvilke podcasts du kan lytte til, for at blive klogere på dette emne. <br>Vi håber du vil lytte med, og være en aktiv del af vores debatter!</p>
+<div>
+    <img src="../images/america.jpg" alt="America-billede">
+    <h2>Made in America</h2>
+    <p>På tværs af Atlanten kan de begivenheder og udviklinger, der præger USA, afgørende ændre Danmark. Vi dykker ned i ti afgørende øjeblikke eller udviklinger i USAs historie. </p>
+</div>
+<div>
+    <img src="../images/ligestilling.jpg" alt="ligestilling-billede">
+    <h2>Langt til ligestilling!</h2>
+    <p>Ændrer det noget politisk, at vi laver memes om hvide, cismænd og deres privillegier? Hvad er fakta bag de følelser vi sidder med, når vi diskuterer samtykke og diversitetskvoter? </p>
+</div>
 
 </html>
