a2a6e17aa5d4197567a141bb941ee89a5bf8d004
kami7277@stud.kea.dk <kami7277@stud.kea.dk>
2021-04-22 11:44:58 +0200

    podcast singleview

--- single-podcast.php
+++ single-podcast.php
@@ -14,10 +14,10 @@ get_header();
         <article class="singlearticle">
             <h2 class="titel"></h2>
             <a href="https://open.spotify.com/" target=”_blank”>
-                <img src="images/download.png" alt="Spotify-ikon">
+                <img src="wp-content/themes/radioloud/images/download.png" alt="Spotify-ikon">
             </a>
             <a href="">
-                <img src="images/download-kopi.png" alt="Afspil-knap">
+                <img src="wp-content/themes/radioloud/images/download-kopi.png" alt="Afspil-knap">
             </a>
             <p class="txt"></p>
             <img src="" alt="" class="billede">
@@ -46,11 +46,19 @@ get_header();
     const dbUrl = "https://neanderpetersen.dk/kea/09_cms/radioloud/wp-json/wp/v2/podcast/" + aktuelpodcast;
     const episodeUrl = "https://neanderpetersen.dk/kea/09_cms/radioloud/wp-json/wp/v2/episoder?per_page=100";
 
+    const container = document.querySelector("#episoder")
+
     async function getJson() {
         console.log("getJson");
         const data = await fetch(dbUrl);
         podcast = await data.json();
+
+        const data2 = await fetch(episodeUrl);
+        episoder = await data2.json();
+        console.log("episoder:", episoder);
+
         visPodcasts();
+        visEpisoder();
     }
 
     function visPodcasts() {
@@ -59,6 +67,28 @@ get_header();
         document.querySelector(".txt").textContent = podcast.beskrivelse;
         document.querySelector(".billede").src = podcast.billede.guid;
 
+    }
+
+    function visEpisoder() {
+        console.log("viserEpisoderne");
+        let temp = document.querySelector("template");
+        episoder.forEach(episode => {
+            console.log("loop id:", aktuelpodcast);
+            if (episode.horer_til_podcast == aktuelpodcast) {
+                console.log("loop kører id:", aktuelpodcast);
+                let klon = temp.cloneNode(true).content;
+                klon.querySelector("h2").textContent = episode.title.rendered;
+                klon.querySelector(".beskrivelse").innerHTML = episode.episodenr;
+
+                klon.querySelector("article").addEventListener("click", () => {
+                    location.href = episode.link;
+                })
+
+                klon.querySelector("button").href = episode.link;
+                console.log("episode", episode.link);
+                container.appendChild(klon);
+            }
+        })
     }
 
     tilbageKnap();
