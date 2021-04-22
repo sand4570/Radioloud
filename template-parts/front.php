4bd05a9ea5b6fe5546a0efd9578d98dbfe3524c9
kami7277@stud.kea.dk <kami7277@stud.kea.dk>
2021-04-22 10:27:22 +0200

    billeder virker

--- template-parts/front.php
+++ template-parts/front.php
@@ -1,40 +1,40 @@
 <html>
 <h3>Seneste nyt</h3>
 <div>
-    <img src="../images/fr%C3%A5s.jpg" alt="Frås-logo">
+    <img src="wp-content/themes/radioloud/images/fras.jpg" alt="Fras-logo">
     <p>Frås: Episode #120</p>
 </div>
 <div>
-    <img src="../images/ligestilling.jpg" alt="Ligestilling-logo">
+    <img src="wp-content/themes/radioloud/images/ligestilling.jpg" alt="Ligestilling-logo">
     <p>Langt til ligestilling: Episode #73</p>
 </div>
 <div>
-    <img src="../images/america.jpg" alt="America-logo">
+    <img src="wp-content/themes/radioloud/images/america.jpg" alt="America-logo">
     <p>Made in America: Episode #54</p>
 </div>
 <div>
-    <img src="../images/george.jpg" alt="GeorgeFloyd-logo">
+    <img src="wp-content/themes/radioloud/images/george.jpg" alt="GeorgeFloyd-logo">
     <p>Geroge Floyd: Episode #13</p>
 </div>
 <div>
-    <img src="../images/alverden.jpg" alt="Hvadialverden-logo">
+    <img src="wp-content/themes/radioloud/images/alverden.jpg" alt="Hvadialverden-logo">
     <p>Hvad i alverden: Episode #39</p>
 </div>
 
 <div>
-    <img src="../images/deunge.jpg" alt="De-unge-billede">
+    <img src="wp-content/themes/radioloud/images/deunge.jpg" alt="De-unge-billede">
     <h2>De unge ved verdens ende</h2>
     <p>En række stærke og intelligente grønlændere, giver deres besyv på ungdomslivet i Grønland.</p>
 </div>
 <h1>I fokus denne måned...</h1>
 <p>I denne måned sætter Radio Loud fokus på kulturforskelle. I sær ser vi det vigtigt at snakke om hvordan unge trives og begår sig i forskellige kulture og samfund rundt omkring i verden. <br>Kig med herunder, for vores forslag til hvilke podcasts du kan lytte til, for at blive klogere på dette emne. <br>Vi håber du vil lytte med, og være en aktiv del af vores debatter!</p>
 <div>
-    <img src="../images/america.jpg" alt="America-billede">
+    <img src="wp-content/themes/radioloud/images/america.jpg" alt="America-billede">
     <h2>Made in America</h2>
     <p>På tværs af Atlanten kan de begivenheder og udviklinger, der præger USA, afgørende ændre Danmark. Vi dykker ned i ti afgørende øjeblikke eller udviklinger i USAs historie. </p>
 </div>
 <div>
-    <img src="../images/ligestilling.jpg" alt="ligestilling-billede">
+    <img src="wp-content/themes/radioloud/images/ligestilling.jpg" alt="ligestilling-billede">
     <h2>Langt til ligestilling!</h2>
     <p>Ændrer det noget politisk, at vi laver memes om hvide, cismænd og deres privillegier? Hvad er fakta bag de følelser vi sidder med, når vi diskuterer samtykke og diversitetskvoter? </p>
 </div>
