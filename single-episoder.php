107f590200fd6c08ee87c24af787032b54cadb7d
sand4570 <sand4570@stud.kea.dk>
2021-04-22 13:02:33 +0200

    single view fix

--- single-episoder.php
+++ single-episoder.php
@@ -27,58 +27,28 @@ get_header();
 
 
 <script>
-    //let podcast;
-    let episoder;
+    let episode;
     let aktuelepisode = <?php echo get_the_ID() ?>;
+    console.log("episode:", aktuelepisode);
 
-    //const dbUrl = "https://neanderpetersen.dk/kea/09_cms/radioloud/wp-json/wp/v2/podcast/" + aktuelpodcast;
-    const episodeUrl = "https://neanderpetersen.dk/kea/09_cms/radioloud/wp-json/wp/v2/episoder?per_page=100" + aktuelepisode;
+    const episodeUrl = "https://neanderpetersen.dk/kea/09_cms/radioloud/wp-json/wp/v2/episoder/" + aktuelepisode;
 
-    //const container = document.querySelector("#episoder")
 
     async function getJson() {
-        //console.log("getJson");
-        //const data = await fetch(dbUrl);
-        //podcast = await data.json();
+        const data = await fetch(episodeUrl);
+        episode = await data.json();
+        console.log("episoder:", episode);
 
-        const data2 = await fetch(episodeUrl);
-        episoder = await data2.json();
-        console.log("episoder:", episoder);
-
-        //visPodcasts();
         visEpisoder();
     }
 
     function visEpisoder() {
         console.log("visPodcasts");
-        document.querySelector(".titel").textContent = episoder.title;
-        document.querySelector(".txt").textContent = episoder.episodenr;
-        document.querySelector(".billede").src = episoder.billede;
+        document.querySelector(".titel").textContent = episode.title.rendered;
+        document.querySelector(".txt").textContent = episode.episodenr;
+        document.querySelector(".billede").src = episode.billede.guid;
 
     }
-
-    //function visEpisoder() {
-    //        console.log("viserEpisoderne");
-    //        let temp = document.querySelector("template");
-    //        episoder.forEach(episode => {
-    //            console.log("loop id:", aktuelpodcast);
-    //            if (episode.horer_til_podcast[0].id == aktuelpodcast) {
-    //                console.log("loop kører id:", aktuelpodcast);
-    //                let klon = temp.cloneNode(true).content;
-    //                klon.querySelector("h3").textContent = episode.title.rendered;
-    //                klon.querySelector("img").src = episode.billede.guid;
-    //                klon.querySelector(".beskrivelse").innerHTML = episode.episodenr;
-    //
-    //                klon.querySelector("article").addEventListener("click", () => {
-    //                    location.href = episode.link;
-    //                })
-    //
-    //                klon.querySelector("button").href = episode.link;
-    //                console.log("episode", episode.link);
-    //                container.appendChild(klon);
-    //            }
-    //        })
-    //        }
 
     tilbageKnap();
 
